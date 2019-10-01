c874a6dfe87bb6901afac141ad54d1879fe3e5a5
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-01 19:53:48 -0400

    Coding bats every day

---
+++ Coding-Bats/src/main/java/com/trilogyed/CodingBats/CodingBatsApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.CodingBats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingBatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingBatsApplication.class, args);
+	}
+
+}

79ff6ef3a9cb45b8c5365eb4aff7c8e0671924b3
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-02 09:15:42 -0400

    This Coding Bat Promotes Thinking

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/rotateLeft3.java
@@ -0,0 +1,16 @@
+package com.frank_uzoka.CodingBats;
+
+public class rotateLeft3 {
+
+    public int[] rotateLeft3(int[] nums) {
+
+        int[] reverse = {nums[1] , nums[2] , nums[0]};
+
+        return reverse;
+
+
+
+    }
+
+
+}

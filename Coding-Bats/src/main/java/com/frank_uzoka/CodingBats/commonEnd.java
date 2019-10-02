941339d6f15a1a5c23a004286f8e3a5e287e5c60
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-01 21:32:21 -0400

    Fun array practice.

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/commonEnd.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/commonEnd.java
@@ -1,4 +1,15 @@
 package com.frank_uzoka.CodingBats;
 
 public class commonEnd {
+
+    public boolean commonEnd(int[] a, int[] b) {
+
+        if(a[0]==b[0] || a[a.length-1] == b[b.length-1]){
+            return true;
+        }
+
+        return false;
+    }
+
+
 }

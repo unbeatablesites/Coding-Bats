663d40ceda4e1b241b67d7ad4fab739c402eff19
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-02 09:20:38 -0400

    This was an easy one!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/reverse3.java
@@ -0,0 +1,14 @@
+package com.frank_uzoka.CodingBats;
+
+public class reverse3 {
+
+    public int[] reverse3(int[] nums) {
+
+        int[] reversed = {nums[2],nums[1],nums[0]} ;
+
+        return reversed;
+
+    }
+
+
+}

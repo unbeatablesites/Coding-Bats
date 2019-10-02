17967378eaa5a04890f55966342d6ecb9c7eb148
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-01 22:31:49 -0400

    Fun array practice.

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/sum3.java
@@ -0,0 +1,17 @@
+package com.frank_uzoka.CodingBats;
+
+public class sum3 {
+    public int sum3(int[] nums) {
+
+        int sum = 0 ;
+
+        for(int i = 0; i < nums.length; i++){
+
+
+            sum += nums[i];
+
+        }
+        return sum;
+    }
+
+}

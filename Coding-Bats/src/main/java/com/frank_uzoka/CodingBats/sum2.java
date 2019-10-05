3149d0db07901eaa9d6cabb59ad16cff63a188bb
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-05 19:02:35 -0400

    This took some effort!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/sum2.java
@@ -0,0 +1,15 @@
+package com.frank_uzoka.CodingBats;
+
+public class sum2 {
+
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{ 10,2,3,4,5,6,7,8,9,11,10 };
+
+        System.out.println( nums[0] + nums[1]);
+
+
+    }
+
+}

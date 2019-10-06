74c5bc7e7e3a991df5e6470cb0c0ec54589c5252
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-05 23:09:30 -0400

    Too easy

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/middleWay.java
@@ -0,0 +1,13 @@
+package com.frank_uzoka.CodingBats;
+
+public class middleWay {
+    public int[] middleWay(int[] a, int[] b) {
+
+
+
+        int[] mid = {a[1],b[1]};
+
+        return mid;
+    }
+
+}

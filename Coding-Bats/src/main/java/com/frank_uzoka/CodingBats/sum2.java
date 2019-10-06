f5b53f000dbfe1eba7412ed8ae212f4fcc91ced0
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-05 23:10:27 -0400

    Some effort

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/sum2.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/sum2.java
@@ -2,14 +2,17 @@ package com.frank_uzoka.CodingBats;
 
 public class sum2 {
 
+    public int sum2(int[] nums) {
 
+        if(nums.length >= 2)
+            return (nums[0] + nums[1]);
+        if(nums.length == 1)
+            return nums[0];
+        return 0;
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{ 10,2,3,4,5,6,7,8,9,11,10 };
-
-        System.out.println( nums[0] + nums[1]);
 
 
     }
+
 
 }

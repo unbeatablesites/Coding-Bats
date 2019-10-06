bb6baf0ea93bdc92ccf776865cf31472cfcde79c
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-05 23:13:27 -0400

    Fun!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/makeEnds.java
@@ -0,0 +1,12 @@
+package com.frank_uzoka.CodingBats;
+
+public class makeEnds {
+    public int[] makeEnds(int[] nums) {
+
+        int[] firstAndLast = {nums[0],nums[nums.length-1]};
+
+        return firstAndLast;
+
+    }
+
+}

7fd1edb845f4bc640c895ac88095813f97d064ec
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-05 23:33:48 -0400

    Fun!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/no23.java
@@ -0,0 +1,22 @@
+package com.frank_uzoka.CodingBats;
+
+public class no23 {
+
+    public boolean no23(int[] nums) {
+
+        if(nums[0] == 2 || nums[0] == 3){
+            return false;
+
+        }
+
+        if(nums[1] == 2 || nums[1] == 3){
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+
+}

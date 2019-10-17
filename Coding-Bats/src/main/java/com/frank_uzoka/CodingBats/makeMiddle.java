b937919b6707a49b88f5bb4d5329c32d0973869e
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-17 00:30:18 -0400

    Fun!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/makeMiddle.java
@@ -0,0 +1,18 @@
+package com.frank_uzoka.CodingBats;
+
+public class makeMiddle {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{ 10,2,3,4,5,6,7,8,9,11,10,5 };
+
+        System.out.println(nums.length/2);
+
+
+    }
+
+
+
+
+
+}

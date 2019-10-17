ea797b08fd6f99ad8a634049f6605739f54ef5ae
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-16 22:48:03 -0400

    Fun!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/fix23.java
@@ -0,0 +1,25 @@
+package com.frank_uzoka.CodingBats;
+
+public class fix23 {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6};
+
+        for(int i = 0; i< array.length; i++){
+
+            System.out.println(array.length);
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}

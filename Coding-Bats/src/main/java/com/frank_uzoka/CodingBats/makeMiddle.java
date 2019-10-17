ec69c0fc27a713d8a1eb5f5189d484322af00af2
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-17 15:19:51 -0400

    Fun!

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/makeMiddle.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/makeMiddle.java
@@ -4,9 +4,18 @@ public class makeMiddle {
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{ 10,2,3,4,5,6,7,8,9,11,10,5 };
+        int[] nums = new int[]{ 10,2,3,4,5,300,7,8,9,11,10,5 };
 
-        System.out.println(nums.length/2);
+        int[] firstSplit = new int[firstSplit.length];
+
+
+        firstSplit[2] = firstSplit[2];
+
+
+        System.out.println(firstSplit[1]);
+
+        System.out.println(nums[1]);
+
 
 
     }

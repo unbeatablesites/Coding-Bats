e941da6605f6063e969ac2deda1ad97ecc887c18
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-16 21:07:20 -0400

    Fun!

---
+++ Coding-Bats/src/main/java/double23.java
@@ -0,0 +1,19 @@
+public class double23 {
+
+    public boolean double23(int[] nums)
+    {
+
+        if(nums.length == 2 && nums[0] == 2 && nums[1] == 2){
+            return true;
+        }
+
+        if (nums.length == 2 && nums[0] == 3 && nums[1] == 3){
+            return true;
+
+        }
+
+
+        return false;
+    }
+
+    }

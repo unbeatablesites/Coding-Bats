92eb573d62f892cf3dce128c02440159955e1a51
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-06 22:02:56 -0500

    Fun with java logic!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/altPairs.java
@@ -0,0 +1,16 @@
+package com.frank_uzoka.CodingBats;
+
+public class altPairs {
+
+    public String altPairs(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i += 4) {
+            int end = i + 2;
+            if (end > str.length()) end = str.length();
+            result += str.substring(i, end);
+        }
+        return result;
+    }
+
+}

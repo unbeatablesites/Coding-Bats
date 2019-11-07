47b04471764ebbcf6d42007ccbc9f9ac072281b6
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-06 21:41:34 -0500

    Fun with java logic!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/frontTimes.java
@@ -0,0 +1,19 @@
+package com.frank_uzoka.CodingBats;
+
+public class frontTimes {
+
+
+    public String frontTimes(String str, int n) {
+        int frontLen = 3;
+        if (frontLen > str.length()) frontLen = str.length();
+        String front = str.substring(0, frontLen);
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            result += front;
+        }
+        return result;
+
+    }
+
+}

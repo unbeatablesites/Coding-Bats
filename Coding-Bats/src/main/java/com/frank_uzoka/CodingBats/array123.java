525c74199281751200abf2ea94afebe689c537d8
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-06 21:59:01 -0500

    Fun with java logic!

---
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/array123.java
@@ -0,0 +1,12 @@
+package com.frank_uzoka.CodingBats;
+
+public class array123 {
+
+    public boolean array123(int[] nums) {
+        for (int i = 0; i < (nums.length - 2); i++) {
+            if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3) return true;
+        }
+        return false;
+    }
+
+}

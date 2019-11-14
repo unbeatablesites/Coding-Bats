f7ff40ffb941d362c741ca68221386069237a482
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-13 23:18:00 -0500

    Add Tales Field

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
@@ -1,7 +1,16 @@
 package com.frank_uzoka.CodingBats;
 
+import org.springframework.web.server.session.HeaderWebSessionIdResolver;
+
 public class CoinFlipApp {
 
-    public String Heads;
+    public static void main(String[] args) {
+
+         String heads = "Heads";
+        String tales = "Tales";
+        System.out.println(heads);
+        System.out.println(tales);
+    }
+
 
 }

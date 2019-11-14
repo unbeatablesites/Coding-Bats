70bde1a0787f814ced7ba5aa18117e635814b08d
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-13 23:20:40 -0500

    Bring in random util

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
@@ -2,12 +2,17 @@ package com.frank_uzoka.CodingBats;
 
 import org.springframework.web.server.session.HeaderWebSessionIdResolver;
 
+import java.util.Random;
+
 public class CoinFlipApp {
 
     public static void main(String[] args) {
 
-         String heads = "Heads";
+        String heads = "Heads";
         String tales = "Tales";
+
+        Random rnd = new Random();
+
         System.out.println(heads);
         System.out.println(tales);
     }

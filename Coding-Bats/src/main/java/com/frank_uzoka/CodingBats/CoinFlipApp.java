c812a375f72316660b7b54a88a7ce3b0eb53fd68
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-15 21:58:10 -0500

    Add tales logic

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
@@ -21,6 +21,16 @@ public class CoinFlipApp {
                 System.out.println("heads was the correct guess");
             }else if(i == 2){
                 System.out.println("tales was the correct guess");
+            }else System.out.println("You did not enter heads or tales sorry");
+        }
+
+        if(userInput.equals("tales")){
+            System.out.println("You Picked Tales");
+            int i = rnd.nextInt((2 - 1) + 1) + 1;
+            if(i == 1){
+                System.out.println("Tales was the correct guess");
+            }else if(i == 2){
+                System.out.println("Heads was the correct guess sorry");
             }else System.out.println("You did not enter heads or tales");
         }
 

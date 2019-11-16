ebbdfa14187cfc1d9fa86eb1599708d71e3116f5
Frank Uzoka <Frankuzoka@hotmail.com>
2019-11-15 22:04:48 -0500

    Give the user another chance before the game ends

--- Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
+++ Coding-Bats/src/main/java/com/frank_uzoka/CodingBats/CoinFlipApp.java
@@ -8,42 +8,42 @@ import java.util.Scanner;
 public class CoinFlipApp {
 
     public static void main(String[] args) {
-        Random rnd = new Random();
-        String userInput = null;
-        System.out.println("Please enter Heads of Tales");
 
-        Scanner Scan = new Scanner(System.in);
-        userInput = Scan.nextLine();
-        if(userInput.equals("heads")){
-            System.out.println("You Picked Heads");
-            int i = rnd.nextInt((2 - 1) + 1) + 1;
-            if(i == 1){
-                System.out.println("heads was the correct guess");
-            }else if(i == 2){
-                System.out.println("tales was the correct guess");
-            }else System.out.println("You did not enter heads or tales sorry");
-        }
 
-        if(userInput.equals("tales")){
-            System.out.println("You Picked Tales");
-            int i = rnd.nextInt((2 - 1) + 1) + 1;
-            if(i == 1){
-                System.out.println("Tales was the correct guess");
-            }else if(i == 2){
-                System.out.println("Heads was the correct guess sorry");
-            }else System.out.println("You did not enter heads or tales");
-        }
+            Random rnd = new Random();
+            String userInput = null;
+            System.out.println("Please enter Heads of Tales");
 
-        else{
-            System.out.println("Please enter heads or tales");
-        }
+            Scanner Scan = new Scanner(System.in);
+        do {
+            userInput = Scan.nextLine();
+            if (userInput.equals("heads")) {
+                System.out.println("You Picked Heads");
+                int i = rnd.nextInt((2 - 1) + 1) + 1;
+                if (i == 1) {
+                    System.out.println("heads was the correct guess");
+                } else if (i == 2) {
+                    System.out.println("tales was the correct guess");
+                } else System.out.println("You did not enter heads or tales sorry");
+            }
 
+            if (userInput.equals("tales")) {
+                System.out.println("You Picked Tales");
+                int i = rnd.nextInt((2 - 1) + 1) + 1;
+                if (i == 1) {
+                    System.out.println("Tales was the correct guess");
+                } else if (i == 2) {
+                    System.out.println("Heads was the correct guess sorry");
+                } else System.out.println("You did not enter heads or tales");
+            } else {
+                System.out.println("Please enter heads or tales");
+            }
 
 
 //
 
 
-
+        }while (userInput != "heads" ||);
 
     }
 
